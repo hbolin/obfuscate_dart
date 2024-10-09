f3747e187f21b44fb99a3fcc64568589590cf671
zhangwu <hbolin@163.com>
2024-10-09 15:01:52 +0800

    ObfuscateDart

--- lib/obfuscate_dart.dart
+++ lib/obfuscate_dart.dart
@@ -1,7 +1,64 @@
 library obfuscate_dart;
 
+import 'dart:io';
+
+import 'package:analyzer/dart/analysis/results.dart';
+import 'package:analyzer/dart/analysis/utilities.dart' as AnalyzerUtilities; // ignore: library_prefixes
+import 'package:analyzer/src/dart/ast/ast.dart';
+import 'package:dart_format/dart_format.dart';
+
 /// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class ObfuscateDart {
+  /// [interval] 调整的间隔,最好是1-5之间的随机数
+  static void obfuscateMethod(String classPath, int interval) {
+    var classFile = File(classPath);
+    var classString = classFile.readAsStringSync();
+    String unformattedText = _confusionMethod(classString, interval);
+
+    final Config configAll = Config.all();
+    final Formatter formatter = Formatter(configAll);
+    final String formattedText = formatter.format(unformattedText);
+
+    print(formattedText);
+    classFile.writeAsStringSync(formattedText);
+  }
+
+  /// [interval] 调整的间隔,最好是1-5之间的随机数
+  static String _confusionMethod(String s, int interval) {
+    if (interval < 1) {
+      throw "调整的间隔要大于0";
+    }
+    final String sWithoutCarriageReturns = s.replaceAll('\r', '');
+    final ParseStringResult parseResult = AnalyzerUtilities.parseString(content: sWithoutCarriageReturns, throwIfDiagnostics: false);
+    var rawString = parseResult.unit.toSource();
+    for (var declaration in parseResult.unit.declarations) {
+      if (declaration is ClassDeclarationImpl) {
+        List<String> methodStringList = [];
+        for (var value in declaration.members) {
+          if (value is MethodDeclarationImpl) {
+            methodStringList.add(value.toSource());
+          }
+        }
+        if (methodStringList.length > 1) {
+          String swapPosition(String tempString, List<String> methodStringList, int position_1, int position_2) {
+            var placeholder = '______PLACE_HOLDER_TEMP_METHOD______';
+            String swappedCode = tempString
+                .replaceFirst(methodStringList[position_1], placeholder)
+                .replaceFirst(methodStringList[position_2], methodStringList[position_1])
+                .replaceFirst(placeholder, methodStringList[position_2]);
+            return swappedCode;
+          }
+
+          String resultTemp = rawString;
+          for (int i = 0; i < methodStringList.length; i++) {
+            if ((i + interval) < methodStringList.length) {
+              resultTemp = swapPosition(resultTemp, methodStringList, i, i + interval);
+            }
+          }
+          rawString = resultTemp;
+        }
+      }
+    }
+    return rawString;
+  }
 }
